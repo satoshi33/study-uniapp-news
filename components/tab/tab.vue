c4adecde2c3e646b3dfb7694eeb780824ebdf4a7
sy <2243335305@qq.com>
2021-09-12 18:19:10 +0800

    选项卡展示

---
+++ components/tab/tab.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="tab">
+		<scroll-view class="tab-scroll" scroll-x>
+			<view class="tab-scroll__box">
+				<view v-for="(item, index) in list" :key="index" class="tab-scroll__item">{{item.name}}零零落落零零落落</view>
+			</view>
+		</scroll-view>
+
+		<view class="tab-icons">
+			<uni-icons type="gear" size="26" color="#666"></uni-icons>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "tab",
+		data() {
+			return {
+				list: [{
+					name: "语文"
+				}, {
+					name: "数学"
+				}, {
+					name: "英语"
+				}, {
+					name: "数学"
+				}, ]
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.tab {
+		display: flex;
+		width: 100%;
+		border-bottom: 1px #f5f5f5 solid;
+		background-color: #fff;
+
+
+		.tab-scroll {
+			flex: 1;
+			overflow: hidden;
+			box-sizing: border-box;
+			// border: 1px red solid;
+
+			.tab-scroll__box {
+				display: flex;
+				align-items: center;
+				flex-wrap: nowrap;
+				height: 45px;
+				box-sizing: border-box;
+
+				.tab-scroll__item {
+					flex-shrink: 0; // 防止被挤压
+					padding: 0 10px;
+					color: #333;
+					font-size: 14px;
+
+
+				}
+
+			}
+		}
+
+		.tab-icons {
+			position: relative;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 45px;
+
+			&::after { //左侧竖线
+				content: '';
+				position: absolute;
+				top: 12px;
+				bottom: 12px;
+				left: 0;
+				width: 1px;
+				background-color: #ddd;
+			}
+		}
+
+	}
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -2,17 +2,14 @@
 	<view class="content">
 		<!-- 自定义导航栏 -->
 		<navbar></navbar>
+		<!--自定义 tab  -->
+		<tab></tab>
 		<view v-for="item in 100">{{item}}个内容</view>
 	</view>
 </template>
 
 <script>
-	// easyCom components/组件名/组件名.vue 局部引入 不需要import+components
-	// import navbar from '@/components/navbar/navbar.vue'
 	export default {
-		// components: {
-		// 	navbar
-		// },
 		data() {
 			return {
 				title: 'Hello'
